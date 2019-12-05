464370f47aa80ded8107e524a95eaa8418b1afb5
Manuel González <manuel-gonzalez@outlook.com>
2019-12-05 15:14:07 -0300

    commit asesino 100%  Jesús

--- TuCredito_WPF/TuCredito_WPF/TuCreDitModel.Designer.cs
+++ TuCredito_WPF/TuCredito_WPF/TuCreDitModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// La generación de código T4 está habilitada para el modelo 'C:\Users\cvera\Google Drive\Facultad\Programacion c# avanzado\TuCredito_WPF\TuCredito_WPF\TuCredito_WPF\TuCreDitModel.edmx'. 
+﻿// La generación de código T4 está habilitada para el modelo 'C:\Users\manuelg\Source\Repos\cvera310\TuCredito_WPF\TuCredito_WPF\TuCredito_WPF\TuCreDitModel.edmx'. 
 // Para habilitar la generación de código heredada, cambie el valor de la propiedad del diseñador 'Estrategia de generación de código'
 // por 'ObjectContext heredado'. Esta propiedad está disponible en la ventana Propiedades cuando se abre
 // el modelo en el diseñador.
